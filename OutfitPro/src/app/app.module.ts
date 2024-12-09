41547898fc29c1990cbcc597f874cd18cc144128
sarar56 <sararcprojects@gmail.com>
2024-12-09 19:56:04 +0100

    componente head

--- OutfitPro/src/app/app.module.ts
+++ OutfitPro/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component'; 
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent 
+    FooterComponent, 
+    HeadComponent
   ],
   imports: [
     BrowserModule
